e0b193fcbd8d8f1a8a454d9dbdc685c7dd377104
hoaitrungchar <79318706+hoaitrungchar@users.noreply.github.com>
2024-11-09 00:32:00 +0700

    fix bugs and update query based on new schema

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,8 +2,8 @@
 class UserController {
     private $userModel;
 
-    public function __construct() {
-        $this->userModel = new UserModel();
+    public function __construct($db) {
+        $this->userModel = new UserModel($db);
         $this->auth = new Authorization();
     }
 
@@ -24,69 +24,49 @@ class UserController {
                 Response::json(400, ['error' => 'Invalid data']);
                 return;
             }
-            $exist_customer=$this->userModel->getCustomer($data);
+            $exist_customer=$this->userModel->getUser($data);
             if(count($exist_customer)>=1){
-                Response::json(409, ['error' => 'Username existed']);
-                return;
+                return Response::json(409, ['error' => 'Username existed']);
             }
             $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
-            if ($this->userModel->createCustomer($data)) {
-                Response::json(201, ['message' => 'Create customer successfully']);
-            } 
+            $this->userModel->createCustomer($data);
+            return Response::json(201, ['message' => 'Create customer successfully']);
         } catch (Exception $e) {
-            Response::json(500, ['error' => $e->getMessage()]);
+            return Response::json(500, ['error' => $e->getMessage()]);
         }
     }
 
-    public function loginCustomer() {
+    public function login() {
         try {
             $data = Request::getBody();
-            
             if (!Validator::validate($data, [
                 'username' => 'required|min|max',
                 'password' > 'required|min|max'
             ]))  {
-                Response::json(400, ['error' => 'Invalid data']);
-                return;
+                return Response::json(400, ['error' => 'Invalid data']);
             }
-            $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
-            $exist_customer=$this->userModel->loginCustomer($data);
-
-            if (count($exist_customer)==1){
-                $token = $this->auth->encode('customer');
-                Response::json(200, ['message' => 'Create customer successfully',
+    
+            $exist_customer=$this->userModel->getUser($data);
+            if (password_verify($data['password'],$exist_customer[0]['password_'])){
+                $token=[
+                    "sub" => $exist_customer[0]['user_id'],
+                    "exp" => "exp",
+                    "role" => $exist_customer[0]['role_']
+                ];
+                $token = $this->auth->encode($token);
+                return Response::json(200, ['message' => 'Login successfully',
                                      'token'   => $token
                                     ]);
             }
+            else{
+                return Response::json(400, ['message' => 'Wrong password']);
+            }
+            return Response::json(404, ['message' => 'Account not exist']);
+                                     
 
             
         } catch (Exception $e) {
-            Response::json(500, ['error' => $e->getMessage()]);
+            return Response::json(500, ['error' => $e->getMessage()]);
         }
-    }
-
-    public function loginManager() {
-        $data = Request::getBody();
-            
-    if (!Validator::validate($data, [
-            'username' => 'required|min|max',
-            'password' > 'required|min|max'
-        ]))  {
-            Response::json(400, ['error' => 'Invalid data']);
-            return;
-        }
-        $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
-        $exist_manager=$this->userModel->loginManager($data);
-
-        if (count($exist_manager)==1){
-            $token = $this->auth->encode('manager');
-            Response::json(200, ['message' => 'Create manager successfully',
-                                 'token'   => $token
-                                ]);
-    }
-
-        
-    } catch (Exception $e) {
-        Response::json(500, ['error' => $e->getMessage()]);
     }
 }
