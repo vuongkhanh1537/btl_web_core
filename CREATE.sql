88570b19332b73b389b716f7e3e469b8600dc78b
ChienBKAI <chien.nguyenbkai@hcmut.edu.vn>
2024-11-27 16:59:13 +0700

    fixed database

--- CREATE.sql
+++ CREATE.sql
@@ -12,6 +12,12 @@ CREATE table  assign_db.user(
     email varchar(255) NOT NULL unique
 );
 
+
+CREATE table  assign_db.collection_(
+    collection_id int  NOT NULL AUTO_INCREMENT primary key,
+    name_ varchar(255) NOT NULL
+);
+
 CREATE table  assign_db.product(
 	product_id int  NOT NULL AUTO_INCREMENT primary key,
     name_ varchar(255) NOT NULL,
@@ -19,42 +25,13 @@ CREATE table  assign_db.product(
     color varchar(255) NOT NULL,
     brand varchar(255) NOT NULL,
     description_ text NOT NULL,
+    image_path text NOT NULL,
     weight_ int NOT NULL,
     size_ int NOT NULL,
     quantity int NOT NULL,
-    category enum ('Shoes', 'Stocks','Sneaker')
-);
-
-
-CREATE table  assign_db.collection_(
-    collection_id int  NOT NULL AUTO_INCREMENT primary key,
-    name_ varchar(255) NOT NULL
-);
-
-
-CREATE table  assign_db.compriesof(
-    collection_id int  NOT NULL ,
-    product_id int  NOT NULL  ,
-    FOREIGN KEY (collection_id) references collection_(collection_id) On update restrict on delete restrict,
-    FOREIGN KEY (product_id) references product(product_id) On update restrict on delete restrict,
-    CONSTRAINT pk_create PRIMARY KEY (collection_id, product_id)
-
-);
-
-
-
-create table  assign_db.order_(
-	order_id int NOT NULL primary key auto_increment,
-    order_time datetime NOT NULL,
-    shipment_time datetime ,
-    ship_fee double not null,
-    payment_status ENUM('Completed', 'Not Completed', 'Cancelled') not null,
-    payment_method varchar(255) not null,
-    payment_time datetime,
-    status_ ENUM('Completed', 'Shipping', 'Cancelled') not null,
-    address_ varchar(255) not null,
-    user_id int not null,
-    FOREIGN KEY (user_id) references user(user_id) On update restrict on delete restrict
+    category enum ('Shoes', 'Stocks','Sneaker'),
+    collection_id int  NOT NULL,
+    FOREIGN KEY (collection_id) references collection_(collection_id) On update restrict on delete restrict
 );
 
 create table  assign_db.promotion_code(
@@ -68,32 +45,30 @@ create table  assign_db.promotion_code(
     init_quantity int NOT NULL
 );
 
-create table  assign_db.cart(
-	cart_id int NOT NULL primary key
-   
-);
 
-create table assign_db.create_(
-	cart_id int not NULL,
-    user_id int not NULL,
-    foreign key (user_id) references user(user_id) On update restrict on delete restrict,
-    foreign key (cart_id) references cart(cart_id) On update cascade on delete cascade,
-    CONSTRAINT pk_create PRIMARY KEY (cart_id, user_id)
-);
-
-create table  assign_db.make(
-	order_id int not NULL primary key,
-    user_id int not NULL,
-    foreign key (user_id) references user(user_id) On update restrict on delete restrict,
-    foreign key (order_id) references order_(order_id) On update cascade on delete cascade
-);
-
-create table  assign_db.apply_for(
-	order_id int not NULL primary key,
+create table  assign_db.order_(
+	order_id int NOT NULL primary key auto_increment,
+    order_time datetime NOT NULL,
+    shipment_time datetime ,
+    ship_fee double not null,
+    payment_status ENUM('Completed', 'Not Completed', 'Cancelled') not null,
+    payment_method varchar(255) not null,
+    payment_time datetime,
+    status_ ENUM('Completed', 'Shipping', 'Cancelled') not null,
+    address_ varchar(255) not null,
+    user_id int not null,
     promotion_code_id int not NULL,
-    foreign key (order_id) references order_(order_id) On update cascade on delete cascade,
-    foreign key (promotion_code_id) references promotion_code(code_id) On update restrict on delete restrict
+    foreign key (promotion_code_id) references promotion_code(code_id) On update restrict on delete restrict,
+    FOREIGN KEY (user_id) references user(user_id) On update restrict on delete restrict
 );
+
+
+create table  assign_db.cart(
+	cart_id int NOT NULL primary key,
+    user_id int not NULL,
+    foreign key (user_id) references user(user_id) On update restrict on delete restrict
+);
+
 
 create table  assign_db.contain(
 	order_id int not NULL,
@@ -115,15 +90,6 @@ create table  assign_db.consisted(
 
 
 
-CREATE table  assign_db.rate(
-	user_id int  NOT NULL,
-    product_id int NOT NULL,
-    score int NOT NULL,
-    CONSTRAINT pk_rate PRIMARY KEY (user_id, product_id),
-    foreign key (user_id) references user(user_id) On update restrict on delete restrict,
-    foreign key (product_id) references product (product_id) On update restrict on delete restrict
-);
-
 
 CREATE table  assign_db.own (
 	user_id int  NOT NULL,
@@ -140,6 +106,7 @@ Create table assign_db.review(
     content varchar(255) not null,
     time_ datetime not null,
     reviewer_id  int not null,
+    score int NOT NULL,
     constraint pk_review Primary key (product_id, ordinal_number),
 	foreign key (product_id) references product (product_id) On update cascade on delete cascade,
     foreign key (reviewer_id) references user(user_id) On update restrict on delete restrict
